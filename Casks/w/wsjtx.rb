d997548a39e9918a66a1c17b6ce153b25ecd7613
Cassiano Aquino <caquino@users.noreply.github.com>
2024-01-29 11:52:28 +0000

    Update wsjtx.rb

--- Casks/w/wsjtx.rb
+++ Casks/w/wsjtx.rb
@@ -6,7 +6,6 @@ cask "wsjtx" do
   desc "Weak signal ham radio communication package"
   homepage "https://www.physics.princeton.edu/pulsar/K1JT/wsjtx.html"
   url "https://downloads.sourceforge.net/wsjt/wsjtx-#{version}-Darwin.dmg"
-  appcast "https://sourceforge.net/projects/wsjt/rss"
 
   app "WSJT-X.app"
 
