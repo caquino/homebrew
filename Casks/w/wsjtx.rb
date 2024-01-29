11002647576bff56791a64d1000d0ca0ed026ef1
Cassiano Aquino <cassiano.aquino@me.com>
2024-01-29 11:40:25 +0000

    first commit

---
+++ Casks/w/wsjtx.rb
@@ -0,0 +1,25 @@
+cask "wsjtx" do
+  version "2.6.1"
+  sha256 "faad6f35a550cf56095eae5ff2f9d124c63187bc6ecff324170c2ae06c609e3e"
+
+  name "WSJT-X"
+  desc "Weak signal ham radio communication package"
+  homepage "https://www.physics.princeton.edu/pulsar/K1JT/wsjtx.html"
+  url "https://downloads.sourceforge.net/wsjt/wsjtx-#{version}-Darwin.dmg"
+  appcast "https://sourceforge.net/projects/wsjt/rss"
+
+  app "WSJT-X.app"
+
+  livecheck do
+    url :stable
+    strategy :sourceforge
+  end
+
+  zap trash: [
+    "~/Library/Application Support/WSJT-X",
+    "~/Library/Preferences/org.physics.princeton.WSJT-X.plist",
+    "~/Library/Saved Application State/org.physics.princeton.WSJT-X.savedState",
+  ]
+end
+
+
